7fa8f60252313118b02fd3ba0403b13d48fc2f59
sairama-doddi <147602182+SaiRama-Doddi@users.noreply.github.com>
2025-01-23 04:56:11 +0000

    second commit by rama

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ function App() {
       <h1 className="heading">My Contacts</h1>
       <Card
         name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
+        imgURL={contacts[0].imgURL}
+        phone={contacts[0].phone}
         email={contacts[0].email}
       />
       <Card
         name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
+        imgURL={contacts[1].imgURL}
+        phone={contacts[1].phone}
         email={contacts[1].email}
       />
       <Card
         name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
+        imgURL={contacts[2].imgURL}
+        phone={contacts[2].phone}
         email={contacts[2].email}
       />
     </div>
